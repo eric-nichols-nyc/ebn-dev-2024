819b9ced72d9c67bb225c348fa3d4245533021f1
Eric Nichols <ebn646@hotmail.com>
2024-09-05 18:13:56 -0400

    feat: Update project page text color

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -30,7 +30,7 @@ const ProjectsPage = () => {
               <h2 className="text-3xl font-bold tracking-tight bg-gradient-to-r from-[#A07CFE] via-[#FFFFFF] to-[#FFBE7B] bg-clip-text text-transparent sm:text-4xl">
                 Projects
               </h2>
-              <p className="mt-4 text-zinc-100">
+              <p className="mt-4 text-zinc-400">
                 Some of the projects are from work and some are on my own time.
               </p>
             </div>
