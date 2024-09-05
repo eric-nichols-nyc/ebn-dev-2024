c31ef976380319597d0cc6265edcbe6477872391
Eric Nichols <ebn646@hotmail.com>
2024-09-05 17:09:48 -0400

    feat: Remove container class from projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -35,7 +35,7 @@ const ProjectsPage = () => {
           </div>
         </div>
       </div>
-      <div className="container px-4 py-8 mx-auto">
+      <div className=" px-4 py-8 mx-auto">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {allProjects.map((project) => (
             <div key={project.slug}>
