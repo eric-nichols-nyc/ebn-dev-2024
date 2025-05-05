1291d9cc2cbbceb5965e161e377fc92447a46521
eric-nichols-nyc <ebn646@gmail.com>
2025-05-05 17:18:20 -0400

    fix: next config 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import LocalFont from "next/font/local";
 import "./globals.css";
 import Provider from "@/components/layout/provider";
 
@@ -9,10 +8,7 @@ const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
-const calSans = LocalFont({
-  src: "../public/fonts/CalSans-SemiBold.ttf",
-  variable: "--font-calsans",
-});
+
 
 export const metadata: Metadata = {
   title: "Eric's Dev Portfolio",
@@ -29,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+    <html lang="en">
       <body className={`${inter.className}`}>
         <Provider>
           {children}
