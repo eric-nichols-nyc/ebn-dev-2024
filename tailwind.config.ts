6f0e8e45c7e99a532f56c2acab6a92a631d682bb
eric-nichols-nyc <ebn646@gmail.com>
2025-05-05 16:28:42 -0400

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,27 +85,9 @@ const config = {
     				to: {
     					'background-position': '0% 0%'
     				}
-    			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
     			}
     		},
     		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out',
     			'accordion-down': 'accordion-down 0.2s ease-out',
     			'accordion-up': 'accordion-up 0.2s ease-out'
     		}
